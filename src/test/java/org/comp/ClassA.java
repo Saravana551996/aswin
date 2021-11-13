e48d211021c5a6962de11851651eeb3a77c3cb38
Saravana551996 <shravan05051996@gmail.com>
2021-11-13 21:18:22 +0530

    First Time code

---
+++ src/test/java/org/comp/ClassA.java
@@ -0,0 +1,16 @@
+package org.comp;
+
+public class ClassA {
+	public void method1() {
+	// TODO Auto-generated method stub
+
+}
+public void method2() {
+	// TODO Auto-generated method stub
+
+}
+public void method3() {
+	// TODO Auto-generated method stub
+
+}
+}
